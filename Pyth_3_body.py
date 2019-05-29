725017ae0ec53c7c7c4d1468ccf5e2a71cd440b6
Chuckswaggin <micknoore145@gmail.com>
2019-05-29 16:57:06 -0400

    Update Pyth_3_body.py
    Slow progress, but progress nonetheless. Completely inaccurate but at least trajectory can now be viewed

--- Pyth_3_body.py
+++ Pyth_3_body.py
@@ -2,104 +2,91 @@
 import matplotlib.pyplot as plt
 import matplotlib.style as style
 style.use('ggplot')
+import math
 
-G = 1e4
-time_step = 0.01
-AU = 1
-MASS_SCALE = 1
+G = 6.67e-11
+time_step = 86400
+AU = 1.496e11
+MASS_SUN = 1.989E30
+#MASS_EARTH = 5.972e24
 
 # Calculate the gravitational forces between the masses
-def calculate_force(mass3, pos3, acc3, mass4, pos4, acc4, mass5, pos5, acc5):
+def calculate_acc(e_pos, e_acc, sun, s_pos):
     # ADD DIVIDE BY ZERO CHECKS LATER
     # Calculate force between masses 3 and 4
-    if (pos3[0] - pos4[0]) != 0:
-        acc3[0] = -(G * mass3 * mass4) / ((pos3[0] - pos4[0]) ** 2)
+    if (e_pos[0] - s_pos[0]) != 0:
+        e_acc[0] = -(e_pos[0] * G * sun) / \
+        math.sqrt(((e_pos[0] - s_pos[0]) **2) + ((e_pos[1] - s_pos[1]) **2)) **3
     else:
-        acc3[0] = 0
-    if (pos3[1] - pos4[1]) != 0:
-        acc3[1] = -(G * mass3 * mass4) / ((pos3[1] - pos4[1]) ** 2)
+        e_acc[0] = 0
+    if (e_pos[1] - s_pos[1]) != 0:
+        e_acc[1] = -(e_pos[1] * G * sun) / \
+        math.sqrt(((e_pos[0] - s_pos[0]) **2) + ((e_pos[1] - s_pos[1]) **2)) **3
     else:
-        acc3[1] = 0
-    acc4[0] = -1 * acc3[0]
-    acc4[1] = -1 * acc3[1]
-    
-    # Calculate force between masses 3 and 5
-    if (pos5[0] - pos3[0]) != 0:
-        acc5[0] = -(G * mass3 * mass5) / ((pos5[0] - pos3[0]) ** 2)
-    else:
-        acc5[0] = 0
-    if (pos5[1] - pos3[1]) != 0:
-        acc5[1] = -(G * mass3 * mass5) / ((pos5[1] - pos3[1]) ** 2)
-    else:
-        acc5[1] = 0
-    acc3[0] += -1 * acc5[0]
-    acc3[1] += -1 * acc5[1]
-    
-    # Calculate force between masses 4 and 5
-    if (pos4[0] - pos5[0]) != 0:
-        acc4[0] += -(G * mass4 * mass5) / ((pos4[0] - pos5[0]) ** 2)
-        acc5[0] += -(G * mass5 * mass4) / ((pos5[0] - pos4[0]) ** 2)
-    if (pos4[1] - pos5[1]) != 0:
-        acc4[1] += -(G * mass4 * mass5) / ((pos4[1] - pos5[1]) ** 2)
-        acc5[1] += -(G * mass5 * mass4) / ((pos5[1] - pos4[1]) ** 2)
+        e_acc[1] = 0
+    #s_acc[0] = -1 * e_acc[0]
+    #s_acc[1] = -1 * e_acc[1]
 
 # Update the positions of the masses
-def update_pos(pos3, vel3, acc3, pos4, vel4, acc4, pos5, vel5, acc5):
-    vel3[0] += acc3[0] * time_step
-    vel3[1] += acc3[1] * time_step
-    pos3[0] += vel3[0] * time_step
-    pos3[1] += vel3[1] * time_step
-    
-    vel4[0] += acc4[0] * time_step
-    vel4[1] += acc4[1] * time_step
-    pos4[0] += vel4[0] * time_step
-    pos4[1] += vel4[1] * time_step
-    
-    vel5[0] += acc5[0] * time_step
-    vel5[1] += acc5[1] * time_step
-    pos5[0] += vel5[0] * time_step
-    pos5[1] += vel5[1] * time_step
+def verlet_pos(e_pos, e_vel, e_acc):
+    e_pos[0] += (e_vel[0] * time_step) + (0.5 * e_acc[0] * (time_step **2))
+    e_pos[1] += (e_vel[1] * time_step) + (0.5 * e_acc[1] * (time_step **2))
+
+# Update the velocity using velocity Verlet equation
+def verlet_vel(e_vel, e_acc, old_acc):
+    e_vel[0] += 0.5 * (e_acc[0] + old_acc[0]) * time_step
+    e_vel[1] += 0.5 * (e_acc[1] + old_acc[1]) * time_step
 
 def main():
-# Mass unit 3 variable initializations
-    mass3 = 3 * MASS_SCALE
-    pos3 = [-2 * AU,-1 * AU]
-    vel3 = [0, 0]
-    acc3 = [0, 0]
+    # Mass unit 3 variable initializations
+    e_pos = [1 * AU, 0]
+    e_vel = [0, 3e4]
+    e_acc = [0, 0]
+    old_acc = [0, 0]
+    positions = [[1 * AU], [0]]
     # Mass unit 4 positions
-    mass4 = 4 * MASS_SCALE
-    pos4 = [2 * AU, 2 * AU]
-    vel4 = [0, 0]
-    acc4 = [0, 0]
-    # Mass unit 5 positions
-    mass5 = 5 * MASS_SCALE
-    pos5 = [2 * AU, -1 * AU]
-    vel5 = [0, 0]
-    acc5 = [0, 0]
+    sun = MASS_SUN
+    s_pos = [0, 0]
     
     # Create the figure and graph the initial points
     fig, ax = plt.subplots()
-    ax.scatter((pos3[0], pos4[0], pos5[0]), (pos3[1], pos4[1], pos5[1]), \
-               c=('green', 'red', 'blue'))
-    ax.set_xlim(-10 * AU, 10 * AU)
-    ax.set_ylim(-10 * AU, 10 * AU)
+    #plt.ion()
+    #ax.scatter((e_pos[0], s_pos[0]), (e_pos[1], s_pos[1]), \
+    #           c=('green', 'blue'))
+    #ax.plot(x_positions, y_positions, c='green')
+    #ax.scatter(s_pos[0], s_pos[1], c='red')
+    #fig.canvas.draw()
+    #ax.set_xlim(-2 * AU, 2 * AU)
+    #ax.set_ylim(-2 * AU, 2 * AU)
     
-    for i in range(100):
-        calculate_force(mass3, pos3, acc3, mass4, pos4, acc4, mass5, pos5, acc5)
+    calculate_acc(e_pos, e_acc, sun, s_pos)
+    
+    for i in range(365):
         #print('\nAcceleration ' + str(i))
-        #print(list(map(lambda x: format(x, '.1e'), acc3)))
-        #print(list(map(lambda x: format(x, '.1e'), acc4)))
-        #print(list(map(lambda x: format(x, '.1e'), acc5)))
+        #print(list(map(lambda x: format(x, '.1e'), e_acc)))
+        #print(list(map(lambda x: format(x, '.1e'), s_acc)))
         
-        update_pos(pos3, vel3, acc3, pos4, vel4, acc4, pos5, vel5, acc5)
+        verlet_pos(e_pos, e_vel, e_acc)
+        positions[0].append(e_pos[0])
+        positions[1].append(e_pos[1])
         #print('\nPosition ' + str(i))
-        #print(list(map(lambda x: format(x, '.1e'), pos3)))
-        #print(list(map(lambda x: format(x, '.1e'), pos4)))
-        #print(list(map(lambda x: format(x, '.1e'), pos5)))
+        #print(list(map(lambda x: format(x, '.1e'), e_pos)))
+        #print(list(map(lambda x: format(x, '.1e'), s_pos)))
+        
+        old_acc[0] = e_acc[0]
+        old_acc[1] = e_acc[1]
+        calculate_acc(e_pos, e_acc, sun, s_pos)
+        verlet_vel(e_vel, e_acc, old_acc)
     
-        ax.scatter((pos3[0], pos4[0], pos5[0]), (pos3[1], pos4[1], pos5[1]), \
-               c=('green', 'red', 'blue'))
-    print(pos3)
+        #ax.scatter((e_pos[0], s_pos[0]), (e_pos[1], s_pos[1]), \
+        #       c=('green', 'blue'))
+        #ax.clear()
+        #ax.plot(x_positions, y_positions, c='green')
+        #ax.scatter(s_pos[0], s_pos[1], c='red')
+        #fig.canvas.draw()
+    print(e_pos)
+    ax.plot(positions[0], positions[1], c='green')
+    ax.scatter(s_pos[0], s_pos[1], c='red')
     plt.show()
-
+    
 main()
